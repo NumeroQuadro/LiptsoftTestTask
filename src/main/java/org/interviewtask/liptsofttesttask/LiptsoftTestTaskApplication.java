409fb2852053c0360f93c41f3edbb2bdb834763f
Dimon limon <dimabelunin7@gmail.com>
2024-05-08 20:13:57 +0300

    projects created

---
+++ src/main/java/org/interviewtask/liptsofttesttask/LiptsoftTestTaskApplication.java
@@ -0,0 +1,12 @@
+package org.interviewtask.liptsofttesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiptsoftTestTaskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LiptsoftTestTaskApplication.class, args);
+    }
+
+}
